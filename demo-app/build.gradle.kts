0e5becc2ef575fa40362f0978f3f730d36c14165
Martin Vysny <mavi@vaadin.com>
2024-11-21 09:35:38 +0200

    demo-app: initial impl

--- demo-app/build.gradle.kts
+++ demo-app/build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
+    alias(libs.plugins.vaadin)
     application
 }
 
 dependencies {
     implementation(project(":flow-graphics"))
     implementation(libs.vaadin.core)
+    implementation(libs.slf4j.simple)
 
     testImplementation(libs.junit)
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
